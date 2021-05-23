3aeb05eb42b76336f020e3563ff8faaf88feede3
Josias <jkodja@gmail.com>
2021-05-23 09:55:39 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-import { } from "./lib7.js";
+import { selek, selekFn, texto } from "./lib7.js";
 import { dicionário } from "./dicionário.js";
+
+selekFn('ok', 'click', () => {
+    texto({ id: 'bl_1', texto: dicionário[selek('txt').value] ?? "Ainda não temos essa palavra no dicionário" });
+    selek('txt').value = '';
+});

1f95585e22c83e34dd65639b7a965f52f67dfd1a
Josias <jkodja@gmail.com>
2021-05-24 13:26:03 -0300

    .

--- js/home.js
+++ js/home.js
@@ -1,14 +1,20 @@
-import { addClass, criarLista, grid, kreatto, selek, templatr, texto } from "./lib7.js";
+import { addClass, grid, kreatto, selek, seleKlass, templatr, texto } from "./lib7.js";
 import { dicionário2 } from "./dicionário2.js";
 
-templatr('header', { section: { id: 'container' } });
-
-grid('blocos', 2, 'bl_');
+templatr('header', { section: { id: 'cx' } }, { section: { id: 'container' } });
 
 kreatto(
-    { header: 'h1 id="ttl"' }, { '#bl_0': [{ input: { type: 'text', id: 'cx_pesquisa' } }, { input: { type: 'button', id: 'ok', value: '=>' } }] }
+    { header: 'h1 id="ttl"' }, { '#cx': [{ input: { type: 'text', id: 'cx_pesquisa' } }, { input: { type: 'button', id: 'ok', value: '=>' } }] }
 );
 
-addClass({ elems: [selek('bl_0')], classe: ' flex center' }, { elems: [selek('cx_pesquisa'), selek('ok')], classe: ' fix' });
+grid('blocos', 24, 'bl_');
+
+addClass({ elems: [selek('cx')], classe: 'flex center' }, { elems: [selek('cx_pesquisa'), selek('ok')], classe: ' fix' });
 
 texto({ id: 'ttl', texto: 'Dicionário de russo' });
+
+console.log(dicionário2[Object.keys(dicionário2)]);
+
+//[...seleKlass('blocos')].forEach((blc, i) => {
+//    texto({ id: `bl_${i}`, texto: `<h2>${Object.keys(dicionário2)[i]}</h2>` });
+//});

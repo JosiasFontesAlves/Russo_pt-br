b285e7e89fe205a72a44679a901fbd9dce2f4377
Josias <jkodja@gmail.com>
2021-05-24 14:23:52 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-import { addClass, grid, kreatto, selek, seleKlass, templatr, texto } from "./lib7.js";
+import { addClass, criarLista, grid, kreatto, selek, templatr, texto } from "./lib7.js";
 import { dicionário2 } from "./dicionário2.js";
 
 templatr('header', { section: { id: 'cx' } }, { section: { id: 'container' } });
@@ -13,8 +13,3 @@ addClass({ elems: [selek('cx')], classe: 'flex center' }, { elems: [selek('cx_pe
 
 texto({ id: 'ttl', texto: 'Dicionário de russo' });
 
-console.log(dicionário2[Object.keys(dicionário2)]);
-
-//[...seleKlass('blocos')].forEach((blc, i) => {
-//    texto({ id: `bl_${i}`, texto: `<h2>${Object.keys(dicionário2)[i]}</h2>` });
-//});

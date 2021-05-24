13d3fa14da3497ae00e325687198ae59d6b8ad15
Josias <jkodja@gmail.com>
2021-05-24 20:44:03 -0300

    .

--- js/index.js
+++
@@ -1,7 +0,0 @@
-import { selek, selekFn, texto } from "./lib7.js";
-import { dicionário } from "./dicionário.js";
-
-selekFn('ok', 'click', () => {
-    texto({ id: 'bl_1', texto: dicionário[selek('txt').value] ?? "Ainda não temos essa palavra no dicionário" });
-    selek('txt').value = '';
-});
--- js/template.js
+++ js/template.js
@@ -1,39 +1,22 @@
-import { addClass, criarLista, grid, kreatto, sElem, templatr, texto } from "./lib7.js";
+import { addClass, criarLista, grid, kreatto, selek, templatr, texto } from "./lib7.js";
+import { dicionário2 } from "./dicionário2.js";
 
-templatr('header', { section: { id: 'container' } }, 'footer');
-
-grid('blocos', 4, 'bl_');
+templatr('header', { section: { id: 'cx' } }, { section: { id: 'container' } }, 'footer');
 
 kreatto(
-    { header: 'h1 id="title"' },
-    { '#bl_0': [{ input: { id: 'txt', type: 'text' } }, { input: { id: 'ok', type: 'button', value: '=>' } }] },
-    { '#bl_2': [{ h3: { id: 'meses' } }, { section: { id: 'lista_meses' } }] },
-    { '#bl_3': [{ h3: { id: 'diaSem' } }, { section: { id: 'lista_diaSem' } }] },
-    { footer: 'p id="copyright"' }
+    { header: 'h1 id="ttl"' }, { '#cx': [{ input: { type: 'text', id: 'cx_pesquisa' } }, { input: { type: 'button', id: 'ok', value: '=>' } }] }
 );
 
-['header', 'footer'].forEach(el => addClass({ elems: [sElem(el)], classe: 'fix w100' }));
+grid('blocos', 24, 'bl_');
 
-const calendário = {
-    meses: {
-        janeiro: 'январь', fevereiro: 'февраль', março: 'март', abril: 'апрель', maio: 'май', junho: 'июнь',
-        julho: 'июль', agosto: 'август', setembro: 'сентябрь', outubro: 'октябрь', novembro: 'ноябрь', dezembro: 'декабрь'
-    },
-    diaSem: {
-        domingo: 'воскресенье', 'segunda-feira': 'понедельник', 'terça-feira': 'вторник',
-        'quarta-feira': 'среда', 'quinta-feira': 'четверг', 'sexta-feira': 'пятница', sábado: 'суббота'
-    }
-};
+addClass({ elems: [selek('cx')], classe: 'flex center' }, { elems: [selek('cx_pesquisa'), selek('ok')], classe: ' fix' });
 
-function inject([obj, local]) {
-    [...arguments].forEach(e => {
-        for (let m in e[0]) criarLista([e[1], [`${m} - ${e[0][m]}`], 'p']);
-    });
+texto({ id: 'ttl', texto: 'Dicionário de russo' });
+
+let ctrl = 0;
+
+for (let a in dicionário2) {
+    texto({id: `bl_${ctrl++}`, texto: `<h2>${a}</h2> <p id="blc_${ctrl}"></p>`});
+    for (let b in dicionário2[a])
+        criarLista([`blc_${ctrl}`, [`${b} - ${dicionário2[a][b]}`], 'p']);
 }
-
-inject([calendário.meses, 'lista_meses'], [calendário.diaSem, 'lista_diaSem']);
-
-texto(
-    { id: 'title', texto: 'Dicionário de russo' }, { id: 'meses', texto: 'Meses' },
-    { id: 'diaSem', texto: 'Dias da semana' }, { id: 'copyright', texto: 'Matsa &copy; 2021' }
-);

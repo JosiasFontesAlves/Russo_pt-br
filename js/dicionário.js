c29fdac224e2b19914662210c70f68431f7d5650
Josias <jkodja@gmail.com>
2021-05-30 22:37:41 -0300

    Update dicionário.js

--- js/dicionário.js
+++ js/dicionário.js
@@ -1,5 +1,5 @@
-/* 
-    
+/*
+
 */
 
 export const dicionário = {
@@ -7,80 +7,89 @@ export const dicionário = {
         abacate: 'авокадо', abacaxi: 'ананас', abaixar: 'опускать', aberto: 'открытый', abismo: 'бездна', abril: 'апрель', abandonar: 'оставлять',
         acrescentar: 'добавить', adeus: 'до свидания', adoecer: 'заболеть', adormecer: 'заснуть', aeroporto: 'Аэропорт', agosto: 'август',
         agradecer: 'благодарить', água: 'вода', alcançar: 'достать / доставать', alegrar: 'радовать', alegremente: 'весело', além: 'дальше',
-        alimentar: 'кормить', amizade: 'дружба', aparecer: 'появиться', arremessar: 'бросить', arroz: 'рис',
+        alimentar: 'кормить', amizade: 'дружба', aparecer: 'появиться', arremessar: 'бросить', arroz: 'рис', assim: 'так',
     },
     B: {
-        balançar: 'качать', banheiro: 'ванная комната', banquisa: 'льдина', barba: 'борода', barco: 'корабль ', bastante: 'достаточно', batata: 'картошка',
-        bebê: 'Младенец', beber: 'пить', bem: 'хорошо', buraco: 'дырка', buscar: 'искать',
+        baixo: 'короткий', balançar: 'качать', banheiro: 'ванная комната', banquisa: 'льдина', barba: 'борода', barco: 'корабль',
+        brasileiro: 'бразилец(pes)/бразильский(adj)', bastante: 'достаточно', batata: 'картошка', bebê: 'Младенец', beber: 'пить', bem: 'хорошо',
+        bonito: 'красивый', buraco: 'дырка', buscar: 'искать',
     },
     C: {
-        cabeça: 'голова', cabelo: 'волос', caçar: 'ловить', café: 'кофе', cair: 'выпасть', carne: 'мясо', 'carne bovina': 'говядина',
-        'carne de porco': 'свинина', carro: 'машина', casa: 'дом', certo: 'правильный', cerveja: 'пиво', cidade: 'город', comida: 'еда',
-        conhecer: 'встречать', conseguir: 'достичь', cobrir: 'накрывать', como: 'как', congelar: 'замерзнуть', cortar: 'нарезать',
-        considerar: 'рассмотреть', continuar: 'продолжать', chá: 'чай', chamada: 'вызов', chamar: 'звать', chapéu: 'шляпа', chatear: 'наскучить',
-        crescer: 'расти', cume: 'Вершина',
+        cabeça: 'голова', cabelo: 'волос', caçar: 'ловить', café: 'кофе', cair: 'выпасть', cardápio: 'меню', carne: 'мясо', 'carne bovina': 'говядина',
+        'carne de porco': 'свинина', carro: 'машина', casa: 'дом', certo: 'правильный', cerveja: 'пиво', cidade: 'город', cinco: 'пять', coisa: 'вещь',
+        colher: 'ложка', comer: 'есть', comida: 'еда', conhecer: 'встречать', conseguir: 'достичь', cobrir: 'накрывать', como: 'как',
+        congelar: 'замерзнуть', cortar: 'нарезать', considerar: 'рассмотреть', conta: 'счёт', continuar: 'продолжать', chá: 'чай', chamada: 'вызов',
+        chamar: 'звать', chapéu: 'шляпа', chatear: 'наскучить', crescer: 'расти', cume: 'Вершина',
     },
     D: {
-        definitivamente: 'Определённо', 'de repente': 'вдруг', desgraça: 'бедa', despedir: 'прощать', dezembro: 'декабрь', doente: 'больной/болен',
-        domingo: 'воскресенье', durante: 'в течение',
+        definitivamente: 'Определённо', delicioso: 'вкусный', demais: 'слишком', 'de repente': 'вдруг', desgraça: 'бедa', despedir: 'прощать',
+        dez: 'десять', dezembro: 'декабрь', dois: 'два', doente: 'больной/болен', domingo: 'воскресенье', durante: 'в течение',
     },
     E: {
-        endereço: 'Адрес', errado: 'неправый', especialmente: 'особенно', estômago: 'желудок', evidência: 'доказательство', evitar: 'избегать', eu: 'я',
+        e: 'и', ela: 'она', ele: 'он', eles: 'они', em: 'на', endereço: 'Адрес', entender: 'понимать', errado: 'неправый', especialmente: 'особенно',
+        estômago: 'желудок', evidência: 'доказательство', evitar: 'избегать', eu: 'я',
     },
     F: {
-        faca: 'нож', falso: 'Неверный(ая)', faminto: 'голодный', feliz: 'счастливый/счастлив', fevereiro: 'февраль', ficar: 'Остаться', frango: 'курица',
-        fruta: 'фрукт',
+        faca: 'нож', falso: 'Неверный(ая)', fantástico: 'потрясающий', faminto: 'голодный', feio: 'некрасивый', feliz: 'счастливый/счастлив',
+        fevereiro: 'февраль', ficar: 'Остаться', fino: 'тонкий', forte: 'сильный', fraco: 'слабый', frango: 'курица', fruta: 'фрукт',
     },
     G: {
-        garganta: 'горло/глотка', gênio: 'гений', gramado: 'газон', grito: 'крик',
+        garfo: 'вилка', garganta: 'горло/глотка', gênio: 'гений', gordo: 'толстый', gostar: 'нравится', grande: 'большой', gramado: 'газон', grito: 'крик',
     },
     H: {
         homem: 'мужчина', hora: 'час', hospital: 'больница',
     },
     I: {
-        imaginação: 'воображение', insanamente: 'безумно', injusto: 'нечестный', irmãos: 'братья', irritado: 'злой', irritar: 'бесить',
+        imaginação: 'воображение', inglês: 'английский (idioma e adjetivo)', insanamente: 'безумно', injusto: 'нечестный', irmãos: 'братья',
+        irritado: 'злой', irritar: 'бесить', isso: 'оно',
     },
     J: {
         janeiro: 'январь', joelho: 'Колень', junho: 'июнь', julho: 'июль',
     },
     L: {
-        laranja: 'апельсин', legume: 'овощ', lembrar: 'вспоминать', levantar: 'вставать', ligar: 'включать', limão: 'лимон',
+        laranja: 'апельсин', legal: 'классный', legume: 'овощ', leite: 'молоко', lembrar: 'вспоминать', levantar: 'вставать', ligar: 'включать',
+        limão: 'лимон', longo: 'длинный',
     },
     M: {
-        macarrão: 'макароны', maçã: 'яблоко', maio: 'май', março: 'март', matar: 'убить', melhorar: 'поправиться', merda: 'гавно (adj)', mexer: 'двигаться',
+        macarrão: 'макароны', maçã: 'яблоко',magro:'худой', manteiga: 'масло', maio: 'май', março: 'март', maravilhoso: 'чудесный', 
+        maravilhosamente: 'чудесно', matar: 'убить', melhorar: 'поправиться', merda: 'гавно (adj)', mesa: 'стол', mexer: 'двигаться', molho: 'соус', 
         mudar: 'изменить', muito: 'очень', mulher: 'женщина',
     },
     N: {
-        não: 'нет', naturalmente: 'естественно', nativo: 'местный', necessário: 'Необходимо', normal: 'нормально', novembro: 'ноябрь',
+        não: 'не/нет', naturalmente: 'естественно', nativo: 'местный', necessário: 'Необходимо', nojento: 'отвратительный', normal: 'нормально',
+        nós: 'мы', nove: 'девять', novembro: 'ноябрь', número: 'номер',
     },
     O: {
-        obrigado: 'спасибо', observar: 'наблюдать', oferecer: 'предложить', oi: 'привет (informal)', opinar: 'высказать', opinião: 'мнение', ordem: 'приказ',
-        ovo: 'яйцо', outubro: 'октябрь',
+        obrigado: 'спасибо', observar: 'наблюдать', oferecer: 'предложить', oi: 'привет (informal)', oito: 'восемь', opinar: 'высказать', opinião: 'мнение',
+        ordem: 'приказ', ovo: 'яйцо', outubro: 'октябрь',
     },
     P: {
-        pão: 'хлеб', parar: 'остановить(ся)', papel: 'Бумага', pegar: 'Брать / взять', peixe: 'рыба', pele: 'Кожа', 'por favor': 'пожалуйста',
-        pernoitar: 'ночевать', pescador: 'рыбак', pessoa: 'человек', piscina: 'бассейн', precisar: 'должен(а/о/ы)', proteger: 'защищать',
+        pão: 'хлеб', parar: 'остановить(ся)', papel: 'Бумага', pedir: 'заказывать', pegar: 'Брать / взять', peixe: 'рыба', pele: 'Кожа',
+        perfeito: 'идеальный', pernoitar: 'ночевать', pescador: 'рыбак', pessoa: 'человек', pequeno: 'маленький', piscina: 'бассейн',
+        'por favor': 'пожалуйста', precisar: 'должен(а/о/ы)', pronto: 'готов', proteger: 'защищать',
     },
     Q: {
-        quadris: 'бëдра', quadro: 'портрет', 'quarta-feira ': 'среда', quebrar: 'сломать', queijo: 'сыр', querer: 'хотеть', 'quinta-feira ': 'четверг',
+        quadris: 'бëдра', quadro: 'портрет', 'quarta-feira ': 'среда', quatro: 'четыре', que: 'что', quebrar: 'сломать', queijo: 'сыр', querer: 'хотеть',
+        'quinta-feira ': 'четверг',
     },
     R: {
         rainha: 'королева', rasgar: 'рвать', realizar: 'выполнить', realmente: 'действительно', receber: 'получать', reclamar: 'жаловаться', rei: 'король',
-        relatório: 'доклад', reserva: 'бронирование', reservar: 'бронировать', respeitar: 'уважать', rodoviária: 'Автовокзал', roubar: 'украсть',
+        relatório: 'доклад', reserva: 'бронирование', reservar: 'бронировать', respeitar: 'уважать', restaurante: 'ресторан', rodoviária: 'Автовокзал',
+        roubar: 'украсть', russo: 'русский/русская (idioma e adjetivo)',
     },
     S: {
-        salada: 'салат', sauna: 'баня', sair: 'выйти', sábado: 'суббота', 'segunda-feira': 'понедельник', saudade: 'тоска', selo: 'марка', sem: 'без',
-        ser: 'Быть', setembro: 'сентябрь', 'sexta-feira': 'пятница', sim: 'да', sonhar: 'мечтать', sopa: 'суп',
+        salada: 'салат', sauna: 'баня', sair: 'выйти', sábado: 'суббота', 'segunda-feira': 'понедельник', saudade: 'тоска', selo: 'марка', seis: 'шесть',
+        sem: 'без', ser: 'Быть', sete: 'семь', setembro: 'сентябрь', 'sexta-feira': 'пятница', sim: 'да', sonhar: 'мечтать', sopa: 'суп', suco: 'сок',
     },
     T: {
-        talvez: 'может быть', tchau: 'пока пока', tentar: 'попытаться', 'terça-feira': 'вторник', tocar: 'Затронуть', transportar: 'водить',
-        tratamento: 'Лечение', trocar: 'заменить', tu: 'ты', tudo: 'всё',
+        talvez: 'может быть', também: 'тоже', tchau: 'пока пока', telefone: 'телефон', tentar: 'попытаться', 'terça-feira': 'вторник', tocar: 'Затронуть',
+        transportar: 'водить', tratamento: 'Лечение', três: 'три', trocar: 'заменить', tu: 'ты', tudo: 'всё',
     },
     U: {
-        último: 'последний',
+        último: 'последний', um: 'один',
     },
     V: {
-        vaca: 'корова', visão: 'зрение', você: 'вы', votar: 'голосовать',
+        vaca: 'корова', vinho: 'вино', visão: 'зрение', você: 'вы', votar: 'голосовать',
     },
     X: {
         xícara: 'чашка',

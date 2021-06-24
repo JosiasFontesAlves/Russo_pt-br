e8cc6ec5b00c3a23eca89bdf148c0d84c08600a8
Josias Fontes Alves <68723380+JosiasFontesAlves@users.noreply.github.com>
2021-06-23 22:51:08 -0300

    Update dicionário.js

--- js/dicionário.js
+++ js/dicionário.js
@@ -1,30 +1,4 @@
 /*
-roupas:'одежда',  
-юбка saia
-платье vestido
-шорты shorts
-пальто casaco
-пиджак jaqueta
-свитер suéter; malha (roupa)
-шарф cachecol
-шапка gorro
-шляпа chapéu
-костюм terno
-носки meias
-обувь sapatos
-тапки sandálias
-ботинки botas
-кроссовки tênis (calçado)
-luvas: 'перчатки',
-roupas íntimas   :'трусы   ',
-guarda-chuva:'зонт    ',
-urinar:'пописать',
-escrever: 'писать',
-novo:     'новый ', 
- 
-закрытый fechado
-открытый aberto
-
 */
 
 export const dicionário = {
@@ -33,20 +7,21 @@ export const dicionário = {
         acrescentar: 'добавить', adeus: 'до свидания', administrador: 'администратор', adoecer: 'заболеть', adormecer: 'заснуть', aeroporto: 'Аэропорт',
         agosto: 'август', agradecer: 'благдарить', água: 'вода', ajuda: 'помощь', alcançar: 'достать / доставать', alegrar: 'радовать', alegremente: 'всело',
         além: 'дальше', algo: 'что-нибудь', ali: 'там', alimentar: 'кормить', aluno: 'ученик', amarelo: 'жёлтый', amigo: 'друг', amizade: 'дружба',
-        aparecer: 'появиться', aquele: 'тот', aqui: 'тут/здесь', arremessar: 'бросить', arroz: 'рис', arquiteto: 'архитектор', assim: 'так', avô: 'дедушка ',
-        avó: 'бабушка ', azul: 'синий', 'azul claro': 'голубой',
+        aparecer: 'появиться', aquele: 'тот', aqui: 'тут/здесь', arremessar: 'бросить', arroz: 'рис', arquiteto: 'архитектор', assim: 'так',
+        avô: 'дедушка ', avó: 'бабушка ', azul: 'синий', 'azul claro': 'голубой',
     },
     B: {
         baixo: 'короткий', balançar: 'качать', banco: 'банк', banheiro: 'ванная комната', banquisa: 'льдина', bebida: 'напиток', barba: 'борода',
         barco: 'корабль', básico: 'основной', bastante: 'достаточно', batata: 'картошка', bebê: 'Младенец', beber: 'пить', bem: 'хорошо', bola: 'мяч',
-        bonito: 'красивый', branco: 'белый', Brasil: 'Бразилия', brasileiro: 'бразилец(pes)/бразильский(adj)', buraco: 'дырка', buscar: 'искать',
+        bonito: 'красивый', botas: 'ботинки', branco: 'белый', Brasil: 'Бразилия', brasileiro: 'бразилец(pes)/бразильский(adj)', buraco: 'дырка',
+        buscar: 'искать',
     },
     C: {
-        cabeça: 'голова', cabelo: 'волос', caçar: 'ловить', café: 'кофе', cair: 'выпасть', 'caixa eletrônico': 'банкомат', caminhar: 'ходить',
-        cardápio: 'меню', carne: 'мясо', 'carne bovina': 'говядина', 'carne de porco': 'свинина', carro: 'машина', carteira: 'бумажник', casa: 'дом',
-        Cazaquistão: 'Казахстан', certo: 'правильный', cerveja: 'пиво', cidade: 'город', cinco: 'пять', cinza: 'серый', cliente: 'клиент',
-        cobrir: 'накрывать', coisa: 'вещь', colega: 'коллега', colher: 'ложка (subst)', comer: 'есть', comida: 'еда', como: 'как', comprar: 'купить',
-        conhecer: 'встречать', conseguir: 'достичь', congelar: 'замерзнуть', conhecer: 'познакомиться', cor: 'цвет', cortar: 'нарезать',
+        cabeça: 'голова', cabelo: 'волос', caçar: 'ловить', cachecol: 'шарф', café: 'кофе', chapéu: 'шляпа', cair: 'выпасть', 'caixa eletrônico': 'банкомат',
+        caminhar: 'ходить', cardápio: 'меню', carne: 'мясо', 'carne bovina': 'говядина', 'carne de porco': 'свинина', carro: 'машина', carteira: 'бумажник',
+        casa: 'дом', casaco: 'пальто', Cazaquistão: 'Казахстан', certo: 'правильный', cerveja: 'пиво', cidade: 'город', cinco: 'пять', cinza: 'серый',
+        cliente: 'клиент', cobrir: 'накрывать', coisa: 'вещь', colega: 'коллега', colher: 'ложка (subst)', comer: 'есть', comida: 'еда', como: 'как',
+        comprar: 'купить', conhecer: 'встречать', conseguir: 'достичь', congelar: 'замерзнуть', conhecer: 'познакомиться', cor: 'цвет', cortar: 'нарезать',
         considerar: 'рассмотреть', consumidor: 'покупатель', conta: 'счёт', continuar: 'продолжать', copo: 'стакан', chá: 'чай', chamada: 'вызов',
         chamar: 'звать', chapéu: 'шляпа', chatear: 'наскучить', crescer: 'расти', cume: 'Вершина',
     },
@@ -58,17 +33,18 @@ export const dicionário = {
     },
     E: {
         e: 'и', 'é claro': 'конечно', ela: 'она', ele: 'он', eles: 'они', em: 'в/на', endereço: 'Адрес', entender: 'понимать', errado: 'неправый',
-        escola: 'школа', escritório: 'офис', escuro: 'тёмный', especialmente: 'особенно', esporte: 'спорт', esposa: 'жeнa', estádio: 'стадион',
-        este: 'этот', estômago: 'желудок', evidência: 'доказательство', evitar: 'избегать', eu: 'я',
+        escola: 'школа', escrever: 'писать', escritório: 'офис', escuro: 'тёмный', especialmente: 'особенно', esporte: 'спорт', esposa: 'жeнa',
+        estádio: 'стадион', este: 'этот', estômago: 'желудок', evidência: 'доказательство', evitar: 'избегать', eu: 'я',
     },
     F: {
         fábrica: 'завод', faca: 'нож', falar: 'говорить', falso: 'Неверный(ая)', faminto: 'голодный', família: 'семья', fantástico: 'потрясающий',
-        farmácia: 'аптека', favorito: 'любимый', feio: 'некрасивый', feliz: 'счастливый/счастлив', fevereiro: 'февраль', ficar: 'Остаться', filha: 'дочь',
-        filho: 'сын', fino: 'тонкий', forte: 'сильный', fraco: 'слабый', frango: 'курица', fruta: 'фрукт',
+        farmácia: 'аптека', favorito: 'любимый', fechado: 'закрытый', feio: 'некрасивый', feliz: 'счастливый/счастлив', fevereiro: 'февраль',
+        ficar: 'Остаться', filha: 'дочь', filho: 'сын', fino: 'тонкий', forte: 'сильный', fraco: 'слабый', frango: 'курица', fruta: 'фрукт',
     },
     G: {
         ganhar: 'выиграть', garfo: 'вилка', garganta: 'горло/глотка', garrafa: 'бутылка', garota: 'девочка ', garoto: 'мальчик ', gênio: 'гений',
-        genitor: 'родитель', gordo: 'толстый', gostar: 'нравится', grande: 'большой', gramado: 'газон', grito: 'крик',
+        genitor: 'родитель', gordo: 'толстый', gorro: 'шапка', gostar: 'нравится', grande: 'большой', gramado: 'газон', grito: 'крик',
+        'guarda-chuva': 'зонт',
     },
     H: {
         homem: 'мужчина', hora: 'час', hospital: 'больница',
@@ -78,20 +54,20 @@ export const dicionário = {
         irritado: 'злой', irritar: 'бесить', isso: 'оно',
     },
     J: {
-        janeiro: 'январь', joelho: 'Колень', junho: 'июнь', julho: 'июль',
+        janeiro: 'январь', jaqueta: 'пиджак', joelho: 'Колень', junho: 'июнь', julho: 'июль',
     },
     L: {
         laranja: 'апельсин', laranjado: 'оранжевый', legal: 'классный', legume: 'овощ', leite: 'молоко', lembrar: 'вспоминать', levantar: 'вставать',
-        ligar: 'включать', livraria: 'книжный магазин', livro: 'книга', limão: 'лимон', longo: 'длинный', loja: 'магазин',
+        ligar: 'включать', livraria: 'книжный магазин', livro: 'книга', limão: 'лимон', longo: 'длинный', loja: 'магазин', luvas: 'перчатки',
     },
     M: {
         mãe: 'мать', macarrão: 'макароны', maçã: 'яблоко', magro: 'худой', maio: 'май', manteiga: 'масло', março: 'март', maravilhoso: 'чудесный',
-        maravilhosamente: 'чудесно', marido: 'муж', marrom: 'коричневый', matar: 'убить', melhorar: 'поправиться', mercado: 'рынок ', merda: 'гавно (adj)',
-        mesa: 'стол', mexer: 'двигаться', molho: 'соус', mudar: 'изменить', muito: 'очень', mulher: 'женщина',
+        maravilhosamente: 'чудесно', marido: 'муж', marrom: 'коричневый', matar: 'убить', meias: 'носки', melhorar: 'поправиться', mercado: 'рынок ',
+        merda: 'гавно (adj)', mesa: 'стол', mexer: 'двигаться', molho: 'соус', mudar: 'изменить', muito: 'очень', mulher: 'женщина',
     },
     N: {
         não: 'не/нет', nada: 'ничего', naturalmente: 'естественно', nativo: 'местный', 'na verdade': 'вообще-то', necessário: 'Необходимо',
-        nojento: 'отвратительный', normal: 'нормально', nós: 'мы', nove: 'девять', novembro: 'ноябрь', número: 'номер',
+        nojento: 'отвратительный', normal: 'нормально', nós: 'мы', nove: 'девять', novembro: 'ноябрь', novo: 'новый ', número: 'номер',
     },
     O: {
         obrigado: 'спасибо', observar: 'наблюдать', oferecer: 'предложить', oi: 'привет (informal)', oito: 'восемь', onde: 'где', onze: 'одиннадцать',
@@ -110,24 +86,26 @@ export const dicionário = {
     R: {
         rainha: 'королева', rasgar: 'рвать', realizar: 'выполнить', realmente: 'действительно', receber: 'получать', reclamar: 'жаловаться', rei: 'король',
         relatório: 'доклад', reserva: 'бронирование', reservar: 'бронировать', respeitar: 'уважать', restaurante: 'ресторан', rodoviária: 'Автовокзал',
-        rosa: 'розовый', roxo: 'фиолетовый', roubar: 'украсть', russo: 'русский/русская (idioma e adjetivo)',
+        rosa: 'розовый', roxo: 'фиолетовый', roubar: 'украсть', roupas: 'одежда', 'roupas íntimas': 'трусы', russo: 'русский/русская (idioma e adjetivo)',
     },
     S: {
-        saber: 'знать', salada: 'салат', sauna: 'баня', sair: 'выйти', sábado: 'суббота', 'segunda-feira': 'понедельник', saudade: 'тоска',
-        secretário: 'Секретарь', selo: 'марка', seis: 'шесть', sem: 'без', ser: 'Быть', sete: 'семь', setembro: 'сентябрь', 'sexta-feira': 'пятница',
-        sim: 'да', sobremesa: 'десерт', sonhar: 'мечтать', sopa: 'суп', sorte: 'удача', suco: 'сок', supermercado: 'супермаркет',
+        saber: 'знать', saia: 'юбка', salada: 'салат', sandálias: 'тапки', sauna: 'баня', shorts: 'шорты', sair: 'выйти', sábado: 'суббота',
+        'segunda-feira': 'понедельник', sapatos: 'обувь', saudade: 'тоска', secretário: 'Секретарь', selo: 'марка', seis: 'шесть', sem: 'без', ser: 'Быть',
+        sete: 'семь', setembro: 'сентябрь', 'sexta-feira': 'пятница', sim: 'да', sobremesa: 'десерт', sonhar: 'мечтать', sopa: 'суп', sorte: 'удача',
+        suco: 'сок', suéter: 'свитер', supermercado: 'супермаркет',
     },
     T: {
-        taça: 'бокал', talvez: 'может быть', também: 'тоже', taxi: 'такси', tchau: 'пока пока', telefone: 'телефон', tentar: 'попытаться',
-        'terça-feira': 'вторник', tocar: 'Затронуть', todo: 'любой (inteiro)', trabalhador: 'рабочий', trabalhar: 'работать', trabalho: 'работа ',
-        transportar: 'водить', tratamento: 'Лечение', três: 'три', treze: 'тринадцать', trocar: 'заменить', tu: 'ты', tudo: 'всё',
+        taça: 'бокал', talvez: 'может быть', também: 'тоже', taxi: 'такси', tchau: 'пока пока', telefone: 'телефон', tênis: 'кроссовки',
+        tentar: 'попытаться', 'terça-feira': 'вторник', terno: 'костюм', tocar: 'Затронуть', todo: 'любой (inteiro)', trabalhador: 'рабочий', trabalhar: 'работать', trabalho: 'работа ', transportar: 'водить', tratamento: 'Лечение', três: 'три', treze: 'тринадцать', trocar: 'заменить', tu: 'ты',
+        tudo: 'всё',
     },
     U: {
-        Ucrânia: 'Украина', último: 'последний', uniforme: 'форма', um: 'один', 'um pouco': 'немного',
+        Ucrânia: 'Украина', último: 'последний', uniforme: 'форма', um: 'один', 'um pouco': 'немного', urinar: 'пописать',
     },
     V: {
         vaca: 'корова', velho: 'старый', vender: 'продать', verde: 'зелёный', 'verde claro': 'светло-зелёный', vermelho: 'красный',
-        'vermelho escuro': 'тёмно-красный', vestir: 'надевать', vinho: 'вино', visão: 'зрение', vinte: 'двадцать', você: 'вы', votar: 'голосовать',
+        'vermelho escuro': 'тёмно-красный', vestido: 'платье', vestir: 'надевать', vinho: 'вино', visão: 'зрение', vinte: 'двадцать', você: 'вы',
+        votar: 'голосовать',
     },
     X: {
         xícara: 'чашка',

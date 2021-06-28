16dbcfa4428717c3b8210a545ecc0a1809540319
Josias <jkodja@gmail.com>
2021-06-28 12:33:07 -0300

    .

--- js/search.js
+++ js/search.js
@@ -3,11 +3,21 @@ import { kreatto, selek, selekFn, templatr, texto } from './lib7.js';
 
 templatr({ main: { class: 'fix' } });
 
-kreatto({
-    main: [{ input: { type: 'text', id: 'txt' } }, { button: { id: 'ok' } }, { p: { id: 'res' } }]
-});
+kreatto(
+    { main: [{ div: { id: 'root', class: 'flex center' } }] },
+    {
+        '#root': [
+            { input: { type: 'text', id: 'txt' } },
+            { button: { id: 'ok' } },
+            { p: { id: 'res', class: 'w100 fix' } }
+        ]
+    }
+);
+
+texto({ id: 'ok', texto: '=>' });
 
 selekFn('ok', 'click', () => {
     const txt = selek('txt');
     texto({ id: 'res', texto: dicionário[txt.value[0].toUpperCase()][txt.value] ?? 'Ainda não temos essa palavra no dicionário' });
+    txt.value = '';
 });

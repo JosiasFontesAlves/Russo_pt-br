fea179c978c2fd7ac6491c13ee16301941e6d7c4
Josias Fontes Alves <68723380+JosiasFontesAlves@users.noreply.github.com>
2021-07-08 10:30:45 -0300

    Update index.js

--- js/game/index.js
+++ js/game/index.js
@@ -4,20 +4,3 @@ templatr(
     { header: { class: 'fix w100' } },
     { div: { id: 'root' } }
 );
-
-
-function render(elem) {
-    for (let tag in elem) {
-        let atr = [];
-
-        for (let key in elem[tag])
-            atr.push(`${a}="${elem[tag][key]}"`);
-
-        return (typeof elem == 'object') ? `<${tag} ${atr.join(' ')}></${tag}>` : `<${elem}></${elem}>`;
-    }
-}
-
-console.log(
-    render({ div: { id: 'root', class: 'mts' } }),
-    render('main')
-);

47d5c5e45bebab9251ebb91157d75b85477f4b4a
Josias <jkodja@gmail.com>
2022-02-04 11:42:20 -0300

    .

--- game/src/js/index.js
+++ game/src/js/index.js
@@ -2,13 +2,10 @@ import { templatr, texto } from './lib7.js';
 import Header from './components/Header.js';
 import Root from './components/Root.js';
 import Footer from './components/Footer.js';
-import setRes from './setRes.js';
 import game from './game.js';
 
 templatr([Header, Root, Footer]);
 
 texto({ 'btn-ok': '=>' });
-
-setRes([]);
 
 game();

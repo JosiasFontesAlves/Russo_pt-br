fcb1fe225601924089f522b0a5da3e320c08b430
Josias <jkodja@gmail.com>
2022-03-29 01:03:48 -0300

    Página de resultados pronta

--- game/src/js/pages/Resultados.js
+++ game/src/js/pages/Resultados.js
@@ -1,7 +1,15 @@
-import { render } from "../lib7.js";
+import { consumirAPI, mapEntries, render, Tabela } from "../lib7.js";
 
-export default render({
-    section: {
-        id: 'resultados'
-    }
-},);
+export default await consumirAPI('api.json', ({ respostas }) => {
+    const TabResultados = mapEntries(respostas, ([data, tab], i) => render({
+        div: {
+            class: 'container-tabs res'
+        }
+    }, [render({ h3: { class: 'ttl_res padd10' } }, data), Tabela(`tab-res-${i}`, tab)]));
+
+    return render({
+        section: {
+            id: 'resultados'
+        }
+    }, [render('h2', 'Resultados anteriores:'), ...TabResultados]);
+});

572d5eafbfb6053d79ca1eb69fdc08a73812743e
Josias Fontes Alves <68723380+JosiasFontesAlves@users.noreply.github.com>
2022-11-13 14:53:18 -0300

    .

--- src/js/App.js
+++ src/js/App.js
@@ -7,13 +7,13 @@ export default () => {
     const fn = {
         'btn-temesc': () => toggle({ body: 'temesc', '#btn-temesc-child': 'x30' }),
         burger: () => toggle({ '#drop': 'drop_hidden' }),
-        res: () => search.removeChild(selek('#res')),
+        res: () => selek('#res').remove(),
         search: () => {
             let str = txt.value.trim();
 
             if (!str) return;
 
-            str = replacer(str, /\w/, str[0].toUpperCase());
+            str = replacer(str, str[0], str[0].toUpperCase());
 
             if (search.children.length > 1) fn.res();
 
@@ -22,8 +22,6 @@ export default () => {
             txt.value = '';
         }
     };
-
-    location.hash = '#home';
 
     selekFn('body', 'click', ev => {
         if (ev.target.localName === 'button') fn[ev.composedPath()[1].id]();
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import Root from './components/Root.js';
 import Footer from './components/Footer.js';
 import App from './App.js';
 
+location.hash = '#home';
+
 templatr(Header, Root, Footer);
 
 App();
